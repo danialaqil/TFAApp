4212ddfc6100080c1164af09939c4eeaeb2f0774
Danial Aqil <danial_aqil@mymail.sutd.edu.sg>
2019-07-01 09:35:01 +0800

    Initial Commit

---
+++ AlarmChartsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlarmChartsApp
+//
+//  Created by Danial Aqil on 1/7/19.
+//  Copyright © 2019 imdadsl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
