d9a290210f8d6b42758fae47d3084b037c3ad066
Danial Aqil <danial_aqil@mymail.sutd.edu.sg>
2019-07-08 16:22:12 +0800

    first

---
+++ AlarmChartsApp/HistoryController.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryController.swift
+//  AlarmChartsApp
+//
+//  Created by Danial Aqil on 8/7/19.
+//  Copyright © 2019 imdadsl. All rights reserved.
+//
+
+import UIKit
+
+class HistoryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
